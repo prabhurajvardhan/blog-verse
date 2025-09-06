4dc5042a6b7d5158bc5e4d221c7f458ec0ad8db5
prabhurajvardhan <prabhurajvardhan@gmail.com>
2025-09-06 16:56:53 +0530

    learnt import export

---
+++ main.js
@@ -0,0 +1,6 @@
+import{add,multiply,subtract} from "./math.js"
+import divide from "./math.js"
+console.log(add(2,5))
+console.log(multiply(5,8))
+console.log(subtract(4,9))
+console.log(divide(8,2))
--- sample.js
+++ sample.js
@@ -32,5 +32,6 @@ let person ={
 
 console.log(person.name)
 console.log(person.age)
+console.log(person[name,age])
 
 console.log(typeof person)
