64ab4d15bb46d8109ec21119725a1fb6693dce3e
prabhurajvardhan <prabhurajvardhan@gmail.com>
2025-09-06 12:34:15 +0530

    learned primitive datatypes

--- sample.js
+++ sample.js
@@ -1,1 +1,36 @@
 console.log("I'm Prabhu Raj Vardhan")
+let name = "I'm learning git"
+
+console.log(name)
+
+let num = 8
+console.log(num)
+
+console.log(typeof name)
+console.log(typeof num)
+
+let islogged = false
+console.log(islogged)
+console.log(typeof islogged)
+
+let x
+console.log(x)
+console.log(typeof x)
+
+let y=null
+console.log(y)
+console.log(typeof y)
+
+let arr = [0,9,8,7,6,5]
+
+console.log(arr[5])
+
+let person ={
+    name:'Prabhu',
+    age:18
+}
+
+console.log(person.name)
+console.log(person.age)
+
+console.log(typeof person)
